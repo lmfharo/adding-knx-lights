ac7134faa4f918a65a01327a69c932fb83aaa244
lmfharo <lauriharo@gmail.com>
2014-11-10 23:46:48 +0200

    Update post_lights.sh

--- post_lights.sh
+++ post_lights.sh
@@ -1,1 +1,1 @@
-cat /tmp/data.txt |  while read line; do curl -X POST -d "$line" --header "Content-Type:application/json" https://houm.herokuapp.com/api/site/:sitekey/light ; done
+cat /tmp/data.txt |  while read line; do curl -X POST -d "$line" --header "Content-Type:application/json" https://<XXX.XXX.com>/api/site/:sitekey/light ; done
